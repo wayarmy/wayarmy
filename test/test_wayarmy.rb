2c2d0c34c42460414f84f9fc235d616742b09e0a
Wayarmy <quanpc@vccloud.vn>
2018-04-13 00:28:40 +0700

    Optimize for test function

--- test/test_wayarmy.rb
+++ test/test_wayarmy.rb
@@ -18,41 +18,28 @@ class WayarmyTest < Minitest::Test
   end
 
   def test_request_code_diferrent_200
-      @r.request
+    @r.request
+    assert_equal 302, @r.request
   end
 
   def test_response_time_greate_than_0
     assert_operator @r.get_response_time, :>, 0
   end
 
-  def test_output_contain_response
-    r = Wayarmy::Main.new()
-    stub_request(:any, "https://gitlab.com").to_return(
-      :status => 302
-    )
-
-    assert_output(/Response code: 302/) { r.start }
-    assert_output(/Response Time/) { r.start }
-    assert_output(/Response Time average:/) { r.start }
-  end
-
-  def test_response_time_will_appear_6_times
+  def test_output_contain_response_and_response_time_appear_6_times
     $stdout = StringIO.new
     @r.start
     string = $stdout.string
     times_appears = string.scan(/(?=#{"Response Time:"})/).count
-
+    assert_match /Response code: 302/, string
+    assert_match /Response Time:/, string
+    assert_match /Response Time average:/, string
     assert_equal 6, times_appears
   end
 
   def test_command_will_run_in_60s
-    r = Wayarmy::Main.new()
-    stub_request(:any, "https://gitlab.com").to_return(
-      :status => 302
-    )
-    
     begin_time = Time.now.to_i
-    r.start
+    @r.start
     complete_time = Time.now.to_i - begin_time
 
     assert_operator complete_time, :>=, 60
